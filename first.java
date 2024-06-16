a38970055e1ae0d80485ee9e146bfa7b6518612a
HardikfromIndia <Hardik64t@gmail.com>
2024-06-16 17:51:49 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+  public static void main(String[] args)
+  
+
+  {
+    int marks = 99;
+    if (marks >= 90)
+      System.out.println("Congrats you got an A");
+    else if(marks >= 80)
+      System.out.println("You have got grade B");
+    else
+      System.out.println("You have to put in more effort");
+    
+  }
+}
